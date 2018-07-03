82c2a2f3160ed2902a808be616790437bac79440
chejianchao <820992376@qq.com>
2018-07-03 06:06:28 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,15 @@ end
 def array_with_two_elements
   return Array.new(2)
 end
+
+def first_element(arr)
+  arr[0]
+end
+
+def third_element(arr)
+  arr[2]
+end
+
+def last_element(arr)
+  arr[-1]
+end
